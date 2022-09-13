560bfc62c6e9e1cc14cefa9f281a70608e3fe16d
Lars Kellogg-Stedman <lars@oddbit.com>
2022-09-13 07:59:06 -0400

    Install trouble

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -70,5 +70,46 @@ return require('packer').startup(function()
   use 'hrsh7th/cmp-cmdline'
   use 'hrsh7th/cmp-vsnip'
   use 'hrsh7th/vim-vsnip'
+  use 'kyazdani42/nvim-web-devicons'
 
+  use {
+      'folke/trouble.nvim',
+      requires = "kyazdani42/nvim-web-devicons",
+      config = function ()
+          require("trouble").setup{
+              icons = false,
+              fold_open = "v", -- icon used for open folds
+              fold_closed = ">", -- icon used for closed folds
+              indent_lines = false, -- add an indent guide below the fold icons
+              signs = {
+                  -- icons / text used for a diagnostic
+                  error = "error",
+                  warning = "warn",
+                  hint = "hint",
+                  information = "info"
+              },
+              use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
+
+          }
+
+          vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
+          {silent = true, noremap = true}
+          )
+          vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
+          {silent = true, noremap = true}
+          )
+          vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
+          {silent = true, noremap = true}
+          )
+          vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
+          {silent = true, noremap = true}
+          )
+          vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
+          {silent = true, noremap = true}
+          )
+          vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
+          {silent = true, noremap = true}
+          )
+      end
+  }
 end)
